33d3386a557f929590a0775d0d1a521c4e92b481
Leticia Rodriguez <lrodriguez@codigodelsur.com>
2021-05-29 15:44:36 -0300

    Initial project setup

---
+++ CustomToastView/CustomToastView.h
@@ -0,0 +1,18 @@
+//
+//  CustomToastView.h
+//  CustomToastView
+//
+//  Created by Leticia Rodriguez on 5/29/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomToastView.
+FOUNDATION_EXPORT double CustomToastViewVersionNumber;
+
+//! Project version string for CustomToastView.
+FOUNDATION_EXPORT const unsigned char CustomToastViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomToastView/PublicHeader.h>
+
+
